9f9aed60e7db450e674fa4eed8ef8994f5419dce
Tina Skov Olesen <tinaskovo@gmail.com>
2021-03-28 19:54:30 +0200

    first final version

--- src/portfolio.jsx
+++ src/portfolio.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import './portfolio.scss'
 import { ReactComponent as ChevronDown } from './icons/chevron-down.svg'
+import { ReactComponent as GitHub } from './icons/github.svg'
+import { ReactComponent as Linkedin} from './icons/linkedin.svg'
+import { ReactComponent as ChevronUp } from './icons/chevron-up.svg'
 import Modal from './modal.jsx'
 import galleryImages from './data/gallery-images.js'
 
@@ -80,11 +83,16 @@ const Portfolio = () => {
 
       <div className="about" id="about">
         <h1>About</h1>
-        <p>
-          Liberal Arts Major turned Code Nerd <br/>
-          I enjoy building things, both privately and professionally <br/>
-          When I'm not coding, you can find me in my allotment in the middle of a DIY project, or cruising around Copenhagen on my vintage moped.
-        </p>
+        <div className="presentation">
+          <div className="profile-image">
+            <img src="https://lh3.googleusercontent.com/B0ivcdApc1AQBvXRkv2Ll0lw49LL79i2t8LhvrXZMU2Ivy8GAXIoXjqTXkjsIy7ZJ3dG1qZSI2bP3w-o5-O7PxcOB3F1_U8fLx0qKvPXOcK9M2rHqo1ha1r_hKCqrSqT0dBPYFQywA=w2400" alt=""/>
+          </div>
+          <p>
+            Liberal Arts Major turned Code Nerd <br/>
+            I enjoy building things, both privately and professionally <br/>
+            When I'm not coding, you can find me in my allotment, in the middle of a DIY project, or cruising around Copenhagen on my vintage moped.
+          </p>
+        </div>
         <a href="#contact" className="scroll-down">
          <p> Contact</p>
           <ChevronDown className="chevron-down" />
@@ -92,9 +100,21 @@ const Portfolio = () => {
       </div>
 
       <div className="contact" id="contact">
-        <h2>Call me</h2>
-        <p>+45 666</p>
-        <a href="#top" className="top-button">To top</a>
+        <h2>Let's keep in touch!</h2>
+        <p><strong>Phone:</strong>  +45 61 65 90 31</p>
+        <div className="social">
+          <a href="https://github.com/tinaskovo">
+            <GitHub className="social-icon"/>
+          </a>
+          <a href="https://www.linkedin.com/in/tinaskovolesen/">
+            <Linkedin className="social-icon"/>
+          </a>
+        </div>
+        <a href="#top" className="top-button">
+          <p>Back to top</p>
+        <ChevronUp className="chevron-up"/>
+        </a>
+
       </div>
     </div>
   )
