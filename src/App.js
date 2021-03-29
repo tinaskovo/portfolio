ca718063d5da37dbe9a1991f73b9c8205cb1b1df
Tina Skov Olesen <tinaskovo@gmail.com>
2021-03-29 15:13:48 +0200

    path added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact component={() => <Portfolio />} />
+        <Route path="/portfolio" exact component={() => <Portfolio />} />
       </Switch>
     </Router>
   );
